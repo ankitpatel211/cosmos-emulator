7542da961299f959b976da282a5cbfe5bfac5556
Ankit Patel <wd.ankit.patel@gmail.com>
2025-06-23 15:17:37 +0530

    use of a cosmos emulator using node.js app

---
+++ app.js
@@ -0,0 +1,21 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+import { CosmosClient } from "@azure/cosmos";
+const cosmosClient = new CosmosClient({
+  endpoint: "https://localhost:8081",
+  key: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+});
+const { database } = await cosmosClient.databases.createIfNotExists({
+  id: "cosmicworks",
+  throughput: 400
+});
+const { container } = await database.containers.createIfNotExists({
+  id: "products",
+  partitionKey: { kind: "Hash", paths: ["/id"] }
+});
+const item = {
+  id: "1",
+  name: "Kiama Classic Surf Board"
+};
+await container.items.upsert(item);
+console.log("Item upserted successfully:", item);
+
